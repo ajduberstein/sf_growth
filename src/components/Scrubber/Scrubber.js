2de647e78fe790c58f2c69bcf91fb40fd5654be2
Andrew Duberstein <ajduberstein@gmail.com>
2018-05-07 23:20:19 -0700

    More reliable scrubber bar

--- src/components/Scrubber/Scrubber.js
+++ src/components/Scrubber/Scrubber.js
@@ -1,91 +1,59 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
 import './Scrubber.css'
 
-const LETTER_WIDTH_PX = 5;
-
-const getMarkSize = () => {
-  try {
-    return document.getElementsByClassName('mark')[0].getBoundingClientRect().width;
-  } catch(error) {
-    return 20
-  }
-}
-
-const getLargest = (marks) => {
-  try {
-    return Math.max(...marks.map(x => ('' + x).length))
-  } catch(error) {
-    return 1
-  }
-}
-
 export default class Scrubber extends Component {
-
-  constructor(props) {
+  constructor (props) {
     super(props)
-    this.onClick = this.onClick.bind(this);
+    this.onClick = this.onClick.bind(this)
   }
 
-  onClick(e) {
-    let selectedIdx = e.target.getAttribute('pk') * 1;
-    e.preventDefault();
-    this.props.handleClick(selectedIdx);
+  onClick (e) {
+    let selectedIdx = e.target.getAttribute('pk') * 1
+    e.preventDefault()
+    this.props.handleClick(selectedIdx)
   }
 
-  render() {
-    let {marks, currentIdx} = this.props;
-    let longestMark = getLargest(marks);
+  render () {
+    let {marks, currentIdx} = this.props
+    console.log(currentIdx)
     if (!marks) return
-    let gridRepeat = marks.length;
-    // let scrubberWidthPx = marks.length * getMarkSize();
-    // let scrubPct = currentIdx / marks.length;
-    let delta = ((LETTER_WIDTH_PX * longestMark) / 2)
-    let currentPlayheadLocation = delta + currentIdx * getMarkSize();
+    let gridRepeat = marks.length
+
     return (
-      <div className="scrubber">
+      <div className='scrubber'>
         <div className='wrapper'>
-          <div className="wrapper markTitleBox"
+          <div className='wrapper markTitleBox'
             style={
               {'gridTemplateColumns': `repeat(${gridRepeat}, 1fr`}
             }>
             {
               marks.map((x, i) => {
                 if (i % 5 === 0) {
-                  return (<div pk={"markTitle" + i}>{x}</div>)
+                  return (<div pk={'markTitle' + i}>{x}</div>)
                 } else {
-                  return (<div pk={"markTitle" + i}>{}</div>)
+                  return (<div pk={'markTitle' + i}>{}</div>)
                 }
               })
             }
           </div>
 
-          <div className="wrapper scrubberBox"
+          <div className='wrapper scrubberBox'
             style={
               {'gridTemplateColumns': `repeat(${gridRepeat}, 1fr`}
-           }>
-           {
+            }>
+            {
               marks.map((x, i) => (
-                  <div pk={i} onClick={this.onClick} className='mark'>
-                    <div pk={i} className='leftBox' />
-                    <div pk={i} className='rightBox' />
-                  </div>
-                )
+                <div pk={i} onClick={this.onClick} className='mark'>
+                  <div pk={i} className='leftBox' />
+                  <div pk={i} className={'rightBox' + (currentIdx === i ? ' circle' : '')} />
+                </div>
+              )
               )
             }
-            <div className='playheadWrapper'>
-              <div className='playhead'>
-                <div className='circle'
-                  style={
-                    {'left': `${currentPlayheadLocation}px` }
-                  } >
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
-
 }
