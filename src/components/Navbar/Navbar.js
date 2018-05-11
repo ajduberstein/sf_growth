ef7d36f374df98f2b204b1439c0f69887cdbddc5
Andrew Duberstein <ajduberstein@gmail.com>
2018-05-11 14:29:54 -0700

    Lint navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,44 +1,42 @@
-import React, {Component} from 'react';
-import './Navbar.css';
-
+import React, {Component} from 'react'
+import './Navbar.css'
 
 export default class Navbar extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
-      selectedLinkName: this.props.links[0] 
+      selectedLinkName: this.props.links[0]
     }
   }
 
-  _onClick(e) {
-    e.preventDefault();
+  _onClick (e) {
+    e.preventDefault()
     let text = e.target.innerText
-    this.props.onClick(text);
+    this.props.onClick(text)
     this.setState({selectedLinkName: text})
   }
 
-  render() {
-    let i = 0;
+  render () {
+    let i = 0
     const links = this.props.links.map((link) => {
-      i++;
+      i++
       let className = 'link'
       if (link === this.state.selectedLinkName) {
         className += ' selected'
       }
       return (
         <span key={i}
-              onClick={this._onClick.bind(this)}
-              className={className}>
+          onClick={this._onClick.bind(this)}
+          className={className}>
           <a href=''>{link}</a>
         </span>
-      );
+      )
     })
     return (
       <div className='navbar'
-      > 
+      >
         {links}
       </div>
-    );
+    )
   }
-
 }
