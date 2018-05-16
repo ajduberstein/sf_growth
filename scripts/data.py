c32e6cb156741ad9077f2042aaa24d247179b31b
Andrew Duberstein <ajduberstein@gmail.com>
2018-05-15 22:08:49 -0700

    Revert "Denormalize input JSON"
    This reverts commit 8057de5b7f9d6c364275dec736c1ab4419539e3e.

--- scripts/data.py
+++ scripts/data.py
@@ -2,33 +2,11 @@ from __future__ import print_function
 
 from datetime import datetime
 import re
-from collections import defaultdict
-import json
 
 import pandas as pd
 
 LAT_REGEX = '37\.\d{1,}'
 LNG_REGEX = '-122\.\d{1,}'
-
-
-def normalize_records(record_collection, key_column_name):
-    time_based_records = defaultdict(list)
-    for record in record_collection:
-        key_column_value = record[key_column_name]
-        record.pop(key_column_name, None)
-        time_based_records[key_column_value].append(record)
-    return time_based_records
-
-
-test_records = [
-    {'hey': 1, 'there': 2},
-    {'hey': 1, 'there': 2},
-    {'there': 3, 'hey': 2}
-]
-assert normalize_records(test_records, 'there') == {
-    2: [{'hey': 1}, {'hey': 1}],
-    3: [{'hey': 2}]
-}
 
 
 def extract(x, regex):
@@ -59,12 +37,12 @@ A['lat'] = pd.to_numeric(A['Business Location'].apply(
     lambda x: extract(x, LAT_REGEX)), errors='coerce')
 A['lng'] = pd.to_numeric(A['Business Location'].apply(
     lambda x: extract(x, LNG_REGEX)), errors='coerce')
-A['start_year'] = A['Location Start Date'].apply(
+A['start_date'] = A['Location Start Date'].apply(
     lambda x: recode_date(x))
 A['end_date'] = A['Location End Date'].apply(
     lambda x: recode_date(x))
 
-COLUMNS = 'lat lng start_year business_name business_type neighborhood_name'
+COLUMNS = 'lat lng start_date business_name business_type neighborhood_name'
 
 # SF bounding box from duberste.in/sql_bounding_box/
 B = A[A['lat'] > 37.7071832174446]
@@ -72,8 +50,5 @@ B = (B[B['lat'] < 37.8396145727521])
 B = B[B['lng'] > -122.56072998046875]
 B = B[B['lng'] < -122.33413696289064]
 B = B[COLUMNS.split(' ')]
-B['lps'] = B['business_name'].str.contains('Juice|Pet |Coffee|Apparel|Yoga')
-records = B.to_dict(orient='records')
-normalized_records = normalize_records(records, 'start_year')
-with open('../public/data/business.json', 'w+') as f:
-    f.write(json.dumps(normalized_records))
+B['vps'] = B['business_name'].str.contains('Juice|Pet |Coffee|Apparel')
+B.to_csv('../public/data/business.csv', index=False)
