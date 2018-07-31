4b59b30c0441a384985e54e673b6f8107168452c
Andrew Duberstein <ajduberstein@gmail.com>
2018-07-31 10:48:31 -0700

    Pull for review script

--- scripts/data_quality/pull_for_review.py
+++ scripts/data_quality/pull_for_review.py
@@ -6,4 +6,4 @@ import pandas as pd
 
 
 A = pd.read_csv('../Registered_Business_Locations_-_San_Francisco.csv')
-A.sample(200)[['NAICS Code Description', 'LIC Code Description']].to_csv('to_manually_label.csv')
+A.sample(200, random_state=42)[['NAICS Code Description', 'LIC Code Description']].to_csv('to_manually_label.csv')
