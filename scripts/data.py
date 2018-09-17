0fc296136aeaf89d0b037932b1a126ec768ae2db
Andrew Duberstein <ajduberstein@gmail.com>
2018-09-17 12:34:39 -0700

    Add neighborhood generation to data.py

--- scripts/data.py
+++ scripts/data.py
@@ -6,7 +6,10 @@ import os
 import re
 
 import numpy as np
+import geopandas as gpd
+from geopandas.tools import sjoin
 import pandas as pd
+from shapely.geometry import Point
 import usaddress
 
 dirname = os.path.dirname(os.path.abspath(__file__))
@@ -17,17 +20,34 @@ LNG_REGEX = '-122\.\d{1,}'
 
 ORDINALS = ['1ST', '2ND', '3RD'] + [str(x) + 'TH' for x in range(4, 10)]
 
+pd.set_option('display.max_columns', 10)
+filepath = '../public/data/neighborhoods.geojson'
+neighborhoods_geom = gpd.read_file(filepath)
+neighborhoods_geom = neighborhoods_geom.set_geometry('geometry')
+
 
 def get_age_in_years(row):
     """Get year a business establishment has been open
     return null if closed
     """
-    if row['business_name'] == 'The Game Parlour':
-        import ipdb; ipdb.set_trace()  # noqa
     if float(row['closed']) == 0:
         return 2018 - float(row['start_date'])
     else:
         return np.nan
+
+
+def _extract_gpd_field_at_idx(gdf, field, idx):
+        poly = gdf.iloc[[idx]]
+        return [x[1][field] for x in poly.iterrows()][0]
+
+
+def get_neighborhood(locations, neighborhoods_geom):
+    points = gpd.GeoDataFrame()
+    points['geom'] = locations.apply(
+        lambda x: Point(x['lng'], x['lat']), axis=1)
+    points = points.set_geometry('geom')
+    result = sjoin(points, neighborhoods_geom, how='left')
+    return result['Name']
 
 
 def clean_address(x):
@@ -184,11 +204,17 @@ def main():
     # remainder.to_csv('to_geocode.csv', index=False)
 
     merged = pd.concat([regex_extract_location[KEEP_COLUMNS], lookup_extract_location[KEEP_COLUMNS]])
-
+    print("Doing point in poly calculation")
+    merged['neighborhood'] = get_neighborhood(merged, neighborhoods_geom)
     merged['age_in_years'] = merged.apply(get_age_in_years, axis=1)
     print("Before duplicate drop", merged.count())
-    merged = merged.drop_duplicates()
+    KEEP_COLUMNS.extend(['neighborhood', 'age_in_years'])
+    merged = merged[KEEP_COLUMNS].drop_duplicates()
     print("After duplicated drop", merged.count())
     fpath = os.path.join(dirname, '../public/data/business.csv')
     print('Writing to ' + fpath)
     merged.to_csv(fpath, index=False)
+
+
+if __name__ == '__main__':
+    main()
